61ded23fa90cf4e0b74509d1c7f1281498329e00
Piotr Pawlowski <ppiotru@gmail.com>
2022-02-03 11:40:29 +0100

    cleanup
    Signed-off-by: Piotr Pawlowski <ppiotru@gmail.com>

--- goSrc/src/vmCaller/iroha/commands.go
+++ goSrc/src/vmCaller/iroha/commands.go
@@ -22,7 +22,6 @@ var (
 	IrohaCommandExecutor unsafe.Pointer
 	IrohaQueryExecutor   unsafe.Pointer
 	Caller               string
-	IrohaErrorDetails    string
 	StoragePointer       unsafe.Pointer
 )
 
@@ -643,7 +642,6 @@ func handleErrors(result *C.Iroha_CommandError, err error, commandName string) (
 		if error_extra_ptr != nil {
 			error_extra = ": " + *error_extra_ptr
 		}
-		IrohaErrorDetails = fmt.Sprintf("%s %s error_code %d ", commandName, error_extra, result.error_code)
 		return fmt.Errorf("Error executing %s command: %s error_code %d", commandName, error_extra, result.error_code)
 	}
 	return nil
